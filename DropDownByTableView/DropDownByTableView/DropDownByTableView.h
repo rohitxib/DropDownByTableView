af8b30d7959101ec999d11a737967da981be0305
Aplite_Mac <aplite_mac@Aplite-Macs-Mac-mini.local>
2022-10-07 10:48:50 +0530

    i have ony created project in side git

---
+++ DropDownByTableView/DropDownByTableView/DropDownByTableView.h
@@ -0,0 +1,18 @@
+//
+//  DropDownByTableView.h
+//  DropDownByTableView
+//
+//  Created by Aplite_Mac on 07/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DropDownByTableView.
+FOUNDATION_EXPORT double DropDownByTableViewVersionNumber;
+
+//! Project version string for DropDownByTableView.
+FOUNDATION_EXPORT const unsigned char DropDownByTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DropDownByTableView/PublicHeader.h>
+
+
